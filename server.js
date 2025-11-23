661e85533e995203fa57b0cf1e7b2ccb8915c102
Sambhu Ganesan <sambhu.ganesan150@gmail.com>
2025-11-23 04:04:56 -0800

    updated server.js

--- server.js
+++ server.js
@@ -23,22 +23,16 @@ app.use(express.static(__dirname));
 
 // API endpoint for Firebase config
 app.get('/api/config', (req, res) => {
-    console.log('=== CONFIG ENDPOINT HIT ===');
-    console.log('All env vars:', Object.keys(process.env));
-    console.log('FIREBASE_API_KEY:', process.env.FIREBASE_API_KEY);
-    console.log('FIREBASE_PROJECT_ID:', process.env.FIREBASE_PROJECT_ID);
-
     const config = {
-        apiKey: process.env.FIREBASE_API_KEY,
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID,
-        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+        apiKey: process.env.FIREBASE_CLIENT_API_KEY,
+        authDomain: process.env.FIREBASE_CLIENT_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_CLIENT_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_CLIENT_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_CLIENT_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_CLIENT_APP_ID,
+        measurementId: process.env.FIREBASE_CLIENT_MEASUREMENT_ID
     };
 
-    console.log('Config being sent:', config);
     res.json(config);
 });
 
